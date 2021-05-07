08eff2baa1adfe0386044cb1899d7f31b917b1e0
yog <65287094+yogeshsanjaray@users.noreply.github.com>
2021-05-07 20:11:47 +0530

    Update users.js

--- end_end/routes/users.js
+++ end_end/routes/users.js
@@ -7,9 +7,6 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get("/dontalk",function (req,res,next) {
-  res.send("speech is silver and silent is gollogind")
-})
 
 router.post('/register',userController.addUser);
 router.post('/login',userController.login);
